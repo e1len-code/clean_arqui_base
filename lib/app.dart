6181b4d7230741a79d62fd813dfdb810e7a96003
e1len-code <160895@unsaac.edu.pe>
2022-08-08 18:17:25 -0500

    feat: add main, injections,app.dart. Add yaml

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CleanArquiBase extends StatelessWidget {
+  const CleanArquiBase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
