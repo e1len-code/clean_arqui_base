84a937d21aee56d89c81e29c58be8cf51a3e7e30
e1len-code <160895@unsaac.edu.pe>
2022-08-24 23:48:00 -0500

    feat:add auths injections

--- lib/injections.dart
+++ lib/injections.dart
@@ -1,5 +1,9 @@
 import 'package:clean_arqui_base/core/network/headers.dart';
 import 'package:clean_arqui_base/core/network/network_info.dart';
+import 'package:clean_arqui_base/feature/auth/data/datasources/auth_remote_data.dart';
+import 'package:clean_arqui_base/feature/auth/data/repository/auth_repository_imple.dart';
+import 'package:clean_arqui_base/feature/auth/domain/repository/auth_repository.dart';
+import 'package:clean_arqui_base/feature/auth/domain/usecases/auth_usecase.dart';
 import 'package:clean_arqui_base/feature/auth/presentation/bloc/auth_bloc.dart';
 import 'package:clean_arqui_base/feature/cuenta/data/datasources/cuenta_remote_data.dart';
 import 'package:clean_arqui_base/feature/cuenta/data/repository/cuenta_repository_imple.dart';
@@ -13,21 +17,26 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 final sl = GetIt.instance;
 Future<void> init() async {
-  sl.registerFactory(() => AuthBloc());
   //! =============== BLOC
+  sl.registerFactory(() => AuthBloc(authUseCase: sl()));
   sl.registerFactory(() => CuentaBloc(cuentaUseCase: sl()));
   //! =============== USECASE
   sl.registerLazySingleton(() => CuentaUseCase(repository: sl()));
+  sl.registerLazySingleton(() => AuthUseCase(authRepository: sl()));
   //! =============== REPOSITORY
   sl.registerLazySingleton<CuentaRepository>(
       () => CuentaRepositoryImple(cuentaRemoteData: sl(), networkInfo: sl()));
+  sl.registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImple(remoteData: sl(), networkInfo: sl()));
   //! =============== LOCAL DATA
   //! =============== REMOTE DATA
   sl.registerLazySingleton<CuentaRemoteData>(
       () => CuentaRemoteDataImple(client: sl()));
+  sl.registerLazySingleton<AuthRemoteData>(
+      () => AuthRemoteDataImple(client: sl()));
   //! =============== CORE
   sl.registerLazySingleton<NetworkInfo>(
-      () => NetworkInfoImpl(sl(), 'http://190.117.145.12/WebApiInnova'));
+      () => NetworkInfoImpl(sl(), 'https://api-userspdf.herokuapp.com'));
   sl.registerLazySingleton(() => Headers());
   //! =============== EXTERNAL
   sl.registerLazySingleton(() => client.Client());
